18bd79000ec2cc884bdd39899c32ac16b993a2e1
Didier Nsengiyumva <nsedidier@gmail.com>
2026-03-07 09:15:46 +0200

    Add explicit OPTIONS handler for CORS preflight

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// Handle preflight requests
+app.options('*', cors(corsOptions));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
