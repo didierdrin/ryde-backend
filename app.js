c8956eec756ccc63cf6e333cee934f1e8bd08300
Didier Nsengiyumva <nsedidier@gmail.com>
2026-03-07 09:37:44 +0200

    Use manual CORS headers instead of middleware

--- app.js
+++ app.js
@@ -20,15 +20,18 @@ var chatsRouter = require('./routes/chats');
 
 var app = express();
 
-// CORS configuration - allow all origins
-app.use(cors({
-  origin: true,
-  credentials: true,
-  optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
-}));
-app.options('*', cors());
+// CORS - Must be first middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
